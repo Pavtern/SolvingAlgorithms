2e46f2bbb02a63ff1afff84ca52622cc853dfef0
Pavtern <ternavskii.pavel@gmail.com>
2020-08-18 11:15:10 +0300

    Initial commit

---
+++ src/StringBits.java
@@ -0,0 +1,41 @@
+/*Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+
+        stringBits("Hello") → "Hlo"
+        stringBits("Hi") → "H"
+        stringBits("Heeololeo") → "Hello"*/
+
+//
+//public class StringBits {
+//    public String str = "Heeololeo";
+//    String result = "";
+//    public static void main (String) {
+//
+//        System.out.println(result);
+//
+//
+//        void setResult() {
+//        for (int i = 0; i < str.length(); i = i + 2) {
+//            result += str.charAt(i);
+//        }
+//
+//    }
+//    }
+//}
+public class StringBits{
+    public String str = "Heeololeo";
+    public String result = "";
+    public static void main(String[] args) {
+        StringBits stringBits = new StringBits();
+        stringBits.setResult();
+        stringBits.getResult();
+    }
+    void setResult() {
+        for (int i = 0; i < str.length(); i = i + 2) {
+            result += str.charAt(i);
+        }
+    }
+    void getResult() {
+        System.out.println(result);
+    }
+}
