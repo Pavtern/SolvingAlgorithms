9c68158e4cbcb521a879050ee2bff2486f10eae9
Pavtern <ternavskii.pavel@gmail.com>
2020-09-01 11:14:43 +0300

    StringX1

--- src/StringX.java
+++ src/StringX.java
@@ -1,3 +1,11 @@
+/*
+Given a string, return a version where all the "x" have been removed. Except an "x" at the very start or end should not be removed.
+
+
+stringX("xxHxix") → "xHix"
+stringX("abxxxcd") → "abcd"
+stringX("xabxxxcdx") → "xabcdx"
+ */
 public class StringX {
     String str = "xHxjkfx";
     String result = "";
