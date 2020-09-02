e7e47efd7c2f4c1b5c1b568897fdff26ddb52b27
Pavtern <ternavskii.pavel@gmail.com>
2020-09-02 13:33:05 +0300

    adding solutions

---
+++ src/noTriples.java
@@ -0,0 +1,19 @@
+public class noTriples {
+    int[] nums = {1,1,1};
+    int n = 0;
+
+    public static void main(String[] args) {
+        noTriples noTriples = new noTriples();
+        noTriples.getRes();
+
+    }
+
+    void getRes(){
+        for (int i = 0; i < nums.length - 2; i++){
+            if((nums[i] == nums[i+1]) &&  (nums[i+1] == nums[i+2])){
+                n = 1;
+            }
+        }
+        System.out.println(n == 0);
+    }
+}
